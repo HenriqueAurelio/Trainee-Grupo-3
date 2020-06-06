5feffb282eabd08482e3b9bb534cffe43b071e66
Pedro Lima <pedroclufjf@gmail.com>
2020-06-06 14:56:56 -0300

    Terminado javascrip para a paginacao

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,45 @@
-function activeBtn() {
-	
-	var x = document.getElementsByClassName("activation-crud-buttons");
-	if (x.className === "page-item activation-crud-buttons"){
-		x.className += "active";
-	} else{
-		x.className = "page-item activation-crud-buttons";
-	}
+function actpago(){
+    var pag1 = document.getElementById("actpago");
+    var pag2 = document.getElementById("actpags");
+    var pag3 = document.getElementById("actpagt");
+    var pagl1 = document.getElementById("actpagol");
+    var pagl2 = document.getElementById("actpagsl");
+    var pagl3 = document.getElementById("actpagtl");
+    if(pag1.className === "page-item actpago"){
+        pag1.className += " active";
+        pag2.className = "page-item actpags";
+        pagl2.className = "page-link pagination-crud-buttons";
+        pag3.className = "page-item actpagt";
+        pagl3.className = "page-link pagination-crud-buttons";
+    }
+}
+function actpags(){
+    var pag1 = document.getElementById("actpago");
+    var pag2 = document.getElementById("actpags");
+    var pag3 = document.getElementById("actpagt");
+    var pagl1 = document.getElementById("actpagol");
+    var pagl2 = document.getElementById("actpagsl");
+    var pagl3 = document.getElementById("actpagtl");
+    if(pag2.className === "page-item actpags"){
+        pag2.className += " active";
+        pag1.className = "page-item actpago";
+        pagl1.className = "page-link pagination-crud-buttons";
+        pag3.className = "page-item actpagt";
+        pagl3.className = "page-link pagination-crud-buttons";
+    }
+}
+function actpagt(){
+    var pag1 = document.getElementById("actpago");
+    var pag2 = document.getElementById("actpags");
+    var pag3 = document.getElementById("actpagt");
+    var pagl1 = document.getElementById("actpagol");
+    var pagl2 = document.getElementById("actpagsl");
+    var pagl3 = document.getElementById("actpagtl");
+    if(pag3.className === "page-item actpagt"){
+        pag3.className += " active";
+        pag2.className = "page-item actpags";
+        pagl2.className = "page-link pagination-crud-buttons";
+        pag1.className = "page-item actpago";
+        pagl1.className = "page-link pagination-crud-buttons";
+    }
 }
