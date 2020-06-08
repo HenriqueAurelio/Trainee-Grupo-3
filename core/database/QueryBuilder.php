0c110a5e1759e55468a0776e4facf9dcf65b1236
Henrique <henriqueaureliocsilva@gmail.com>
2020-06-08 13:28:17 -0300

    exemplo de php e estrutura de dados e configuracao

---
+++ core/database/QueryBuilder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Core\Database;
+
+use PDO;
+
+class QueryBuilder
+{
+
+    protected $pdo;
+
+
+    public function __construct()
+    {
+    
+    }
+
+
+    public function selectAll()
+    {
+      
+    }
+
+    public function insert()
+    {
+      
+         
+    }
+}
