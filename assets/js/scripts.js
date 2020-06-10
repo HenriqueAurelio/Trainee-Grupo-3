f081db0d6c0cd0cf59923fa228a500191941e267
rodrigofayer <rodrigofayer26@gmail.com>
2020-06-10 10:07:41 -0300

    Função de ver a senha

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,15 @@
 function passview(){
     var eyespass = document.getElementById("senhaicon");
-
+    var revpass = document.getElementById("revpass");
     if (eyespass.className === "fas fa-eye"){
         eyespass.className = "fas fa-eye-slash";
     } else {
         eyespass.className = "fas fa-eye";
     }
+    if(revpass.type === "password"){
+        revpass.type = "text";
+    }else {
+        revpass.type = "password";
+    }
+    
 }
