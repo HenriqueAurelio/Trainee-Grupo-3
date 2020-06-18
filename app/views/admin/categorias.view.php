ae6367fea70262248c641fc139c03f6b958c44d4
RodrigoFayer <rodrigofayer26@gmail.com>
2020-06-18 19:31:03 -0300

    Nova branch e pastas organizadas e melhora no layout

--- app/views/admin/categorias.view.php
+++ app/views/admin/categorias.view.php
@@ -19,16 +19,18 @@
         <div class = "container font-crud">
             <h1 class="mt-5 pt-3 start-crud">Categorias</h1>
             <div class="row">
-                    <div class="col-md-6" id="add-crud-responsive">
+                    <div class="col-md-3" id="add-crud-responsive">
                         <a href="categorias/cadastrar"><button type="button" class="btn newbox-add-crud btn-crud btn-lg mb-5 mt-5 cabecalho-crud"><i class="fas fa-plus mr-3"></i>Adicionar</button><a>
                     </div>
-                    <div class="col-md-6" id="search-bar-crud">    
-                        <div class="input-group mt-5 mb-5 responsive-search-crud float-right">
-                            <span class = "procurar-crud">Pesquisar:</span><input type="text" class="form-control float-right newbox-crud input-crud-responsive cabecalho-crud ml-3" placeholder="Categorias" aria-label="Recipient's username" aria-describedby="basic-addon2">
+            </div>
+            <div class="row d-flex justify-content-end">
+                    <div class="col-md-4" id="search-bar-crud">    
+                        <div class="input-group mt-5 mb-5 responsive-search-crud ">
+                            <span class = "procurar-crud">Pesquisar:</span><input type="text" class="form-control  newbox-crud input-crud-responsive cabecalho-crud ml-3" placeholder="Categoria" aria-label="Recipient's username" aria-describedby="basic-addon2">
                         </div>
                     </div>
             </div>
-            <div class = "table-responsive">
+            <div class = "table-sm-responsive">
                 <table class="table table-striped table-bordered">
                     <thead>
                         <tr>
@@ -40,16 +42,21 @@
                       <?php foreach ($categorias as $categoria) : ?>
                         <tr>
                             <td class="font-adjustment-crud"><?= $categoria->nome; ?></td>
-                            <td class="font-adjustment-crud">
-                              <form method="POST" action="categorias/mostrar">
-                                <input type="hidden" name="id" value="<?= $categoria->id ?>">
-                                <button type="submit" class="btn btn-view-crud btn-crud-table newbox-crud btn-sm view-table-crud"><i class="fas fa-eye adjust-eyeicon-crud"></i></button>
+                            <td class="border-right-0">
+                              <div class="row">
+                              <form  method="POST" action="categorias/mostrar" class="ml-4">
+                                <input  class="btn-crud-table" type="hidden" name="id" value="<?= $categoria->id ?>">
+                                <button type="submit" class="btn btn-view-crud btn-crud-table newbox-crud btn-sm view-table-crud vasco"><i class="fas fa-eye adjust-eyeicon-crud"></i></button>
                               </form>
-                              <form method="POST" action="categorias/editar">
-                                <input type="hidden" name="id" value="<?= $categoria->id ?>">
-                                <button type="submit" class="btn btn-edit-crud btn-crud-table newbox-crud btn-sm ml-2"><i class="fas fa-edit mr-1"></i></button>
+                            
+                              <form method="POST" action="categorias/editar" >
+                                <input class="btn-crud-table" type="hidden" name="id" value="<?= $categoria->id ?>">
+                                <button type="submit" class="btn btn-edit-crud btn-crud-table newbox-crud btn-sm ml-2 vasco"><i class="fas fa-edit mr-1"></i></button>
                               </form>
-                              <button type="button" class="btn btn-delete-crud btn-crud-table newbox-crud btn-sm ml-2" data-toggle="modal" data-target="#exampleModalCenter <?= $i; ?>" ><i class="far fa-trash-alt"></i></button>
+                              <form class="crudform">
+                                <button type="button" class="btn btn-delete-crud btn-crud-table newbox-crud btn-sm ml-2 vasco" data-toggle="modal" data-target="#exampleModalCenter <?= $i; ?>" ><i class="far fa-trash-alt"></i></button>
+                              </form>
+                              </div>
                             </td>
                             
                             <div class="modal fade" id="exampleModalCenter <?= $i; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
