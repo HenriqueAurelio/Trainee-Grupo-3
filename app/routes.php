aa1fab8d4bcc1f262efcf5af5e34525ad70a4670
gabriel <gabrielbandrade5@yahoo.com>
2020-06-19 11:11:45 -0300

    modificada a pasta crudusuario

--- app/routes.php
+++ app/routes.php
@@ -28,3 +28,15 @@ $router->post('informacoes-produto', 'ProductsController@produtoInfo');
 $router->get('contato', 'PagesController@contato');
 
 $router->get('admin/home', 'PagesController@admin');
+
+$router->get('usuarios', 'UsersController@index');
+$router->post('usuarios', 'UsersController@store');
+
+$router->get('usuarios/cadastrar', 'UsersController@usersCadastro');
+$router->post('index', 'UsersController@store');
+$router->post('usuarios/deletar', 'UsersController@delete');
+$router->post('usuarios/editar', 'UsersController@edituser');
+$router->post('usuarios/editado', 'UsersController@update');
+$router->post('usuarios/visualizar', 'UsersController@show');
+$router->get('usuarios/voltar', 'UsersController@usersreturn');
+
