540fe93f95cb06bd58e1c58eb212b6117ef4fbcc
Pedrox42 <pedroclufjf@gmail.com>
2020-06-25 02:36:00 -0300

    Arrumado bug do js na navbar

--- app/views/site/products.view.php
+++ app/views/site/products.view.php
@@ -25,8 +25,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../public/css/styles.css">
+   <link rel="stylesheet" href="../../public/css/styles.css">
     <link href="https://fonts.googleapis.com/css2?family=Carrois+Gothic&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/307d809775.js" crossorigin="anonymous"></script>
     <script type="text/javascript" src="../assets/js/scripts.js"></script>
@@ -160,9 +159,6 @@
 
             <!-- JavaScript (Opcional) -->
             <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
-            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <!--Fim Pagina Produtos-->
   </body>
 </html>
