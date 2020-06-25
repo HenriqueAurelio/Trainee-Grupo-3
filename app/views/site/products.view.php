2c41915154befa226197e1d2111f59753c9220f7
Pedrox42 <pedroclufjf@gmail.com>
2020-06-25 18:11:04 -0300

    Arrumado fundo azul da produtos, imagem do tamanho do card

--- app/views/site/products.view.php
+++ app/views/site/products.view.php
@@ -85,7 +85,7 @@
                                                 <div class="card prcards">
                                                             <form method="POST" action="/informacoes-produto">
                                                                 <input name="id" type="hidden" value="<?= $produto->id; ?>">
-                                                                <button type="submit" class="btn btn-primary"><img class="card-img-top tamanhocard" src="../../public/img/<?= $produto->foto; ?>" alt="Imagem de capa do card"></button>
+                                                                <button type="submit" class="border-0 btn-img-produto"><img class="card-img-top tamanhocard" src="../../public/img/<?= $produto->foto; ?>" alt="Imagem de capa do card"></button>
                                                             </form>
                                                         <div class="card-body rescard">
                                                                 <h5 class="card-title tituloCard"><?= $produto->nome; ?></h5>
